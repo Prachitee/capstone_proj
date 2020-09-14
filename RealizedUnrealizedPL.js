e32f8ebc0edf0be42451bf761ad1a857a4ba1e67
ApoorvaNandurdikar <32422258+ApoorvaNandurdikar@users.noreply.github.com>
2020-09-14 15:41:25 +0530

    Add files via upload

---
+++ RealizedUnrealizedPL.js
@@ -0,0 +1,38 @@
+import { Table, Tag, Space, Rate } from 'antd';
+import React, { PureComponent } from 'react';
+import ReactDOM from 'react-dom';
+
+const { Column, ColumnGroup } = Table;
+
+const data = [
+  {
+    key: '1',
+    Symbol : 'MSFT', 
+    Name : 'Microsoft Corp',
+    UnrealizedProfit : '5600',
+    RealizedProfit : '7000',
+    AccruedInterest : '5.6%'
+
+  },
+  
+];
+
+export default class ExampleRealizedUnrealizedPL extends PureComponent {
+    
+    render() {
+        return (
+            <Table dataSource={data}>
+           <Table width={3400} height={100} data={data}>
+            <Column title="Symbol" dataIndex="Symbol" key=" Symbol" />
+            <Column title="Name" dataIndex="Name" key="Name" />
+            <Column title="UnrealizedProfit" dataIndex="UnrealizedProfit" key="UnrealizedProfit" />
+            <Column title="RealizedProfit" dataIndex="RealizedProfit" key="RealizedProfit" />
+            <Column title="AccruedInterest" dataIndex="AccruedInterest" key="AccruedInterest" />
+    
+    
+    
+  </Table>
+          </Table>
+        );
+      }
+}
